29c6cd31bde8ed1f3ba2be475b79d39d96be71c8
Iketyke <isaac.mellad.pemberton@gmail.com>
2022-11-14 11:39:25 +0000

    set up project

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS nc_games_test;
-DROP DATABASE IF EXISTS nc_games;
+DROP DATABASE IF EXISTS ikes_games_test;
+DROP DATABASE IF EXISTS ikes_games;
 
-CREATE DATABASE nc_games_test;
-CREATE DATABASE nc_games;
+CREATE DATABASE ikes_games_test;
+CREATE DATABASE ikes_games;
